e6816ca2794212e9d677cbb8f4aeef837b8070cc
frienddrive6-afk <frienddrive6@gmail.com>
2026-01-16 12:42:59 +0200

    Создан проект по c# добавлено дз

---
+++ lesson1/classwork/Program.cs
@@ -0,0 +1,103 @@
+﻿
+
+
+#region start
+// Console.WriteLine("Hello, World!");
+
+// for(int i = 0; i < 10; ++i)
+// {
+//     Console.WriteLine(i);
+// }
+
+
+// string name = Console.ReadLine();
+
+// Console.BackgroundColor = ConsoleColor.Red;
+// Console.ForegroundColor = ConsoleColor.Black;
+
+// Console.WriteLine("Hellow " + name);
+
+// Console.ResetColor();
+
+// Console.WriteLine("Hellow " + name);
+
+// Console.SetCursorPosition(1, 1);
+
+// System.Console.WriteLine("Hello, World!");
+#endregion
+
+
+#region vars
+
+// int a = 567;
+
+// int b = 0;
+
+// Console.WriteLine(a.ToString() + " " + b.ToString());
+
+// int c = Convert.ToInt32(a.ToString());
+
+// System.Console.WriteLine(c.GetType());
+
+
+#endregion
+
+
+#region  types
+
+//================= Значимые типы ==================
+//храянтся в стеке
+
+// bool
+bool a = true; //or false
+
+//byte
+byte b = 255; // от 0 до 255
+
+// sbyte -128 до 127
+sbyte c = -128;
+
+//short (-32768 до 32767)
+short d = -32768;
+
+//ushort (0 до 65535)
+ushort e = 65535;
+
+//int (-2147483648 до 2147483647)
+int f = -2147483648;
+
+//uint (0 до 4294967295)
+uint g = 4294967295;
+
+//long (-9223372036854775808 до 9223372036854775807)
+long h = -9223372036854775808;
+
+//ulong (0 до 18446744073709551615)
+ulong i = 18446744073709551615;
+
+//float (2b)
+float j = 3.14f;
+
+//double (8b)
+double k = 3.14;
+
+//decimal (16b)
+decimal l = 3.14m;
+
+//char (2b)     Unicode
+char m = 'a';
+char n = '✅';
+System.Console.WriteLine(n);
+
+
+
+
+//================= Ссылочные типы ==================
+//в стеке только сылка на объект в куче
+
+//классы
+string s  = "Vasia";
+
+object q = 34;
+
+#endregion
---
+++ lesson1/homework/Program.cs
@@ -0,0 +1,46 @@
+﻿
+Console.WriteLine("=======================================\nКАЛЬКУЛЯТОР СКИДОК\n=======================================\n\n");
+
+Console.Write("Введите сумму покупки (в монетах): ");
+int countMonay = 0; 
+int.TryParse(Console.ReadLine(), out countMonay);
+
+bool isHaveDiskontCard = false;
+Console.Write("Есть ли у вас карта лояльности? (+/-):");
+string? input = Console.ReadLine();
+if (string.Equals(input, "да", StringComparison.OrdinalIgnoreCase) || input == "+")
+{
+    isHaveDiskontCard = true;
+}
+
+if(countMonay > 0)
+{
+    System.Console.WriteLine("---------------------------------------\nРАСЧЕТ СКИДКИ\n---------------------------------------\n");
+    System.Console.WriteLine($"Сумма покупики: {countMonay}мон");
+    byte pocentOfSumm = 0;
+    if(countMonay >= 1001 && countMonay <= 5000)
+    {
+        pocentOfSumm = 5;
+    }else if(countMonay >= 5001 && countMonay <= 10000)
+    {
+        pocentOfSumm = 10;
+    }else if(countMonay >= 10000)
+    {
+        pocentOfSumm = 15;
+    }
+    
+    
+    System.Console.WriteLine($"Скидка по сумме ({pocentOfSumm}%) : -{(float)countMonay / 100 * pocentOfSumm}мон");
+    if(isHaveDiskontCard == true)
+    {
+        System.Console.WriteLine($"Бонус по карте (5%) : +{(float)countMonay / 100 * 5}мон");
+        pocentOfSumm += 5;
+    }else
+    {
+        System.Console.WriteLine($"Бонус по карте (0%) : -0мон");    
+    }
+    
+
+    System.Console.WriteLine($"---------------------------------------\nОбщая скидка:    {pocentOfSumm}%\nИтоговая сумма:    {(float)countMonay - (float)countMonay / 100 * pocentOfSumm}мон\n=======================================");
+
+}
